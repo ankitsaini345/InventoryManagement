530d37784f8a7f33bfa6d02351ca648ad3c80445
ankitsaini345 <--global>
2023-01-16 00:02:25 +0530

    card total amount added

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { firstValueFrom } from 'rxjs';
+import { CardService } from 'src/app/card/card.service';
+import { TxnService } from 'src/app/txn/txn.service';
 import { IProduct } from '../product';
 import { ProductServiceService } from '../product-service.service';
 
@@ -9,7 +13,10 @@ import { ProductServiceService } from '../product-service.service';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  constructor(private productService: ProductServiceService) { }
+  constructor(private productService: ProductServiceService,
+    private cardService: CardService,
+    private txnService: TxnService,
+    private toastService: ToastrService) { }
 
   products: IProduct[] = [];
   filterBy = '';
@@ -18,35 +25,55 @@ export class ProductListComponent implements OnInit {
     this.getProducts();
   }
 
-  getProducts() {
-    this.productService.getProducts().subscribe((data) => {
-      this.products = data;
-      console.log(data);
-    })
+  async getProducts() {
+    // this.productService.getProducts().subscribe((data) => {
+    //   this.products = data;
+    //   console.log(data);
+    // })
+
+    try {
+      this.products = await firstValueFrom(this.productService.getProducts());
+    } catch (error: any) {
+      this.toastService.error(error.message);
+    }
   }
 
-  deleteProduct(_id: any) {
-    this.productService.deleteProduct(_id).subscribe({
-      next: () => {
-        console.log('Product with id ' + _id + ' deleted');
-        this.getProducts();
-      },
-      error: (err) => {
-        console.error('Error while deleting product with id ' + _id);
-        console.error(err);
-      }
-    })
+  async deleteProduct(product: IProduct) {
+    // this.productService.deleteProduct(_id).subscribe({
+    //   next: () => {
+    //     console.log('Product with id ' + _id + ' deleted');
+    //     this.getProducts();
+    //   },
+    //   error: (err) => {
+    //     console.error('Error while deleting product with id ' + _id);
+    //     console.error(err);
+    //   }
+    // })
+
+    try {
+      await firstValueFrom(this.productService.deleteProduct(product._id));
+      this.toastService.success('Product ' + product.name + ' deleted.');
+      await this.getProducts();
+      await firstValueFrom(this.txnService.deleteTxn(product.txnId));
+      let cardInfo = await firstValueFrom(this.cardService.getCard(product.cardHolder));
+        cardInfo.amountDue -= product.cardAmount;
+        cardInfo.totalAmount -= product.cardAmount;
+        await firstValueFrom(this.cardService.updateCard(cardInfo));
+    } catch (error: any) {
+      this.toastService.error(error.message);
+    }
   }
-  cancelProduct(_id: any) {
-    this.productService.deleteProduct(_id).subscribe({
-      next: () => {
-        console.log('Product with id ' + _id + ' deleted');
-        this.getProducts();
-      },
-      error: (err) => {
-        console.error('Error while deleting product with id ' + _id);
-        console.error(err);
-      }
-    })
-  }
+
+  // cancelProduct(_id: any) {
+  //   this.productService.deleteProduct(_id).subscribe({
+  //     next: () => {
+  //       console.log('Product with id ' + _id + ' deleted');
+  //       this.getProducts();
+  //     },
+  //     error: (err) => {
+  //       console.error('Error while deleting product with id ' + _id);
+  //       console.error(err);
+  //     }
+  //   })
+  // }
 }
