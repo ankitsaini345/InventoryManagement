907e279d45d0364f3522f09196dde33a4aca6584
ankitsaini345 <--global>
2023-01-23 16:19:11 +0530

    primeng table, export

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,16 @@
+
 import { Component, OnInit } from '@angular/core';
-import { SpinnerService } from '../spinner/spinner.service';
+
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styles: []
 })
 export class HomeComponent implements OnInit {
-  
-  constructor(private spinnerService: SpinnerService) { }
 
-  ngOnInit(): void {
-  }
-
-  start(){
-    this.spinnerService.startLoading();
-
-    setTimeout(() => {
-      this.spinnerService.finishLoading();
-    }, 3000);
-  }
+    ngOnInit() {
+    }
 
 
 }
